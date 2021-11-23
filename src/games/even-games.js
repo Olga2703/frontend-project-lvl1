0e07bd86c882794fdd1451a403eb61aae8eac2b0
Olga Petrenko <smirnova_olga18@mail.ru>
2021-11-23 15:02:47 +0400

    fix: correct path to file

---
+++ src/games/even-games.js
@@ -0,0 +1,36 @@
+import readlineSync from 'readline-sync';
+
+import getUserName from '../cli.js';
+import isEvenNumber from '../even-parity.js';
+import getRndInteger from '../random-number.js';
+
+const evenGame = () => {
+  console.log('Welcome to the Brain Games!');
+
+  const userName = getUserName();
+  console.log(`Hello ${userName}`);
+
+  console.log('Answer "yes" if the number is even, otherwise answer "no".');
+
+  const COUNT_QUESTIONS = 3;
+  const MAX_NUMBER = 100;
+  const MIN_NUMBER = 0;
+
+  for (let i = 0; i < COUNT_QUESTIONS; i += 1) {
+    const randomNumber = getRndInteger(MIN_NUMBER, MAX_NUMBER);
+    console.log(`Question: ${randomNumber}`);
+    const answer = readlineSync.question('Your answer: ');
+    if (answer === isEvenNumber(randomNumber)) {
+      console.log('Correct!');
+    } else {
+      console.log(`'${answer}' is wrong answer ;(. Correct answer was 
+            '${isEvenNumber(randomNumber)}'`);
+      break;
+    }
+    if (i === 2) {
+      console.log(`Congratulations, ${userName}!`);
+    }
+  }
+};
+
+export default evenGame;
