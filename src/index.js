130822665cce2c6db562891fe58d122d63763873
Olga Petrenko <smirnova_olga18@mail.ru>
2021-11-24 13:24:32 +0400

    feat: add API games

---
+++ src/index.js
@@ -0,0 +1,33 @@
+import readlineSync from 'readline-sync';
+import { car, cdr } from '@hexlet/pairs';
+
+import getUserName from '../src/cli.js';
+
+const COUNT_QUESTIONS = 3;
+
+const newGame = (questionGame, sequenceGame) => {
+  console.log('Welcome to the Brain Games!');
+
+  const userName = getUserName();
+  console.log(`Hello ${userName}`);
+
+  console.log(`${questionGame}`);
+
+  for (let i = 0; i < COUNT_QUESTIONS; i += 1) {
+    const questionAnswer = sequenceGame();
+    console.log(`Question: ${car(questionAnswer)}`);
+    const answer = readlineSync.question('Your answer: ');
+    if (answer === cdr(questionAnswer)) {
+      console.log('Correct!');
+    } else {
+      console.log(`'${answer}' is wrong answer ;(. Correct answer was 
+            '${cdr(questionAnswer)}'`);
+      break;
+    }
+    if (i === 2) {
+      console.log(`Congratulations, ${userName}!`);
+    }
+  }
+};
+
+export default newGame;
