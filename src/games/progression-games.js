9107c6a4ddaf0e5669dacdcca665c68681da859a
Olga Petrenko <smirnova_olga18@mail.ru>
2021-11-25 14:59:56 +0400

    feat: prepare files for games

---
+++ src/games/progression-games.js
@@ -0,0 +1,11 @@
+import newGame from '../index.js';
+
+const questionGame = 'What number is missing in the progression?';
+
+const sequenceGame = () => {};
+
+const progressionGame = () => {
+  newGame(questionGame, sequenceGame);
+};
+
+export default progressionGame;
