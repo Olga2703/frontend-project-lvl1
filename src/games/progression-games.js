52db66c23cad672d77b32b97b47942f2fb605750
Olga Petrenko <smirnova_olga18@mail.ru>
2021-11-26 11:02:20 +0400

    feat: add game brain-progression

--- src/games/progression-games.js
+++ src/games/progression-games.js
@@ -1,8 +1,30 @@
+import { cons } from '@hexlet/pairs';
 import newGame from '../index.js';
 
+const LENGTH_PROGRESSION = 10;
+const MAX_NUMBER = 100;
+const MIN_NUMBER = 0;
+const MAX_STEP = 10;
 const questionGame = 'What number is missing in the progression?';
 
-const sequenceGame = () => {};
+const sequenceGame = () => {
+  const firstNumber = Math.floor(Math.random() * (MAX_NUMBER - MIN_NUMBER + 1)) + MIN_NUMBER;
+  const step = Math.floor(Math.random() * MAX_STEP);
+
+  const arithmeticProgression = [];
+  arithmeticProgression.push(firstNumber);
+  for (let i = 1; i < LENGTH_PROGRESSION; i += 1) {
+    arithmeticProgression.push(arithmeticProgression[i - 1] + step);
+  }
+  const randomElement = Math.floor(Math.random() * LENGTH_PROGRESSION);
+  const hiddenProgression = [...arithmeticProgression];
+  hiddenProgression[randomElement] = '..';
+
+  const question = `Question: ${hiddenProgression.join(' ')}`;
+  const answer = arithmeticProgression[randomElement];
+  const questionAnswer = cons(question, String(answer));
+  return questionAnswer;
+};
 
 const progressionGame = () => {
   newGame(questionGame, sequenceGame);
