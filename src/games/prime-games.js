cda14f229845deb95e62498794fd66e9b8cd1fb1
Olga Petrenko <smirnova_olga18@mail.ru>
2021-11-30 13:21:56 +0400

    feat: add brain-prime game

---
+++ src/games/prime-games.js
@@ -0,0 +1,22 @@
+import { cons } from '@hexlet/pairs';
+import newGame from '../index.js';
+import isPrime from '../isPrime.js';
+
+const MAX_NUMBER = 100;
+const MIN_NUMBER = 0;
+
+const questionGame = 'Answer "yes" if given number is prime. Otherwise answer "no".';
+
+const sequenceGame = () => {
+  const number = Math.floor(Math.random() * (MAX_NUMBER - MIN_NUMBER + 1)) + MIN_NUMBER;
+  const question = `Question: ${number}`;
+  const answer = isPrime(number);
+  const questionAnswer = cons(question, answer);
+  return questionAnswer;
+};
+
+const primeGame = () => {
+  newGame(questionGame, sequenceGame);
+};
+
+export default primeGame;
---
+++ src/isPrime.js
@@ -0,0 +1,14 @@
+const isPrime = (num) => {
+  let i = 2;
+  let j = 0;
+  if (i * i <= num && j !== 1) {
+    if (num % i === 0) {
+      j = 1;
+    } else {
+      i += 1;
+    }
+  }
+  return j === 1 ? 'no' : 'yes';
+};
+
+export default isPrime;
