a44166de8ef2b14409b113522aa28b0558ba30d9
Olga Petrenko <smirnova_olga18@mail.ru>
2021-11-30 14:52:31 +0400

    fix: correct output for error response

--- src/index.js
+++ src/index.js
@@ -21,7 +21,8 @@ const newGame = (questionGame, sequenceGame) => {
       console.log('Correct!');
     } else {
       console.log(`'${answer}' is wrong answer ;(. Correct answer was 
-            '${cdr(questionAnswer)}'. Let's try again, ${userName}`);
+            '${cdr(questionAnswer)}'.`);
+      console.log(`Let's try again, ${userName}`);
       break;
     }
     if (i === 2) {
